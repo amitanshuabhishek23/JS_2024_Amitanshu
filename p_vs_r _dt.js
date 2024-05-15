144774e9c26651212539b86cbcc16f83ae3d7e07
Amitanshu <amitanshu.abhishek@gmail.com>
2024-05-15 08:14:26 +0530

    Add

--- p_vs_r _dt.js
+++ p_vs_r _dt.js
@@ -17,6 +17,8 @@
     // Reference Type 
         let array1=["item1","item2"];
         let array2=array1;
+        console.log(array2===array1); // true  Use this 
+        console.log(array2==array1); // true 
         console.log("Value of array1", array1); // (2) ['item1', 'item2']
         console.log("Value of array2", array2);  // (2) ['item1', 'item2']
         console.log("After Pushing")
@@ -27,7 +29,31 @@
 
     // How are they store in the memory ? 
         // Primitive - Stored in Stack - One has its own space 
-        // Reference - Stored In Heap Memory 
+        // Reference - Stored In Heap Memory and the Stack has pointer the same address of the reference from heap. 
 
+    // To clone array 
+    let firstName5="Amitanshu"
+    let myname12=firstName5.slice(0);
+    console.log(myname12); 
+
+    let array3=["item1","item2"];
+    console.log("Before Slice" ,array3);
+    let array4=array3.slice(0); 
+    console.log("After Slice ",array4); 
+    let array8=array3.slice(0).concat(["iten7","item8"]); 
+    console.log("Adding to addition",array8);
+     
+    let array5 = [].concat(array3,["item6"]); 
+    console.log("After Concat ",array5);  
+
+    // Spread Operator 
+
+    let array6 = [...array5,"item8"  ];
+    console.log(array6===array5); // Pointer is different 
+    console.log("... Spread Operator", array6); 
+
+    let array9 = [...array5,...array6];
+
+// For Loop In Array 
 
 
