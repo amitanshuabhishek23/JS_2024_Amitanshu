3bc4e8252b9d97cc2b630268237ef7d363d3afa9
Amitanshu <amitanshu.abhishek@gmail.com>
2024-05-28 13:45:51 +0530

    iterables_05 Modified

--- iterables_05.js
+++ iterables_05.js
@@ -47,12 +47,44 @@ const firstname1 = "Amiatnshu" ;
 //== Store Data 
 // In set duplicate is not allowed 
 //-- Order is not guaranteed 
-// -- No index based access
+// -- No index based access 
+// Iterable 
 
 const numbers = new Set([1,2,3,4,3]); // Set(4) {1, 2, 3, 4} -- It will ignore the duplicate but doesn't throw the error ; 
 console.log(numbers) // Set(4) {1, 2, 3, 4}
 
+const numbers1 = new Set();
+console.log(numbers1) // Set(0)  Empty set 
+numbers1.add(1); 
+numbers1.add(3); 
+numbers1.add(['item1' , 'item2']); 
+numbers1.add(['item1' , 'item2']);  // Added as it has the diff memory address 
+console.log(numbers1) // Set(2) {1, 3} 
 
+if(numbers1.has(1)){
+    console.log("1 is present..!!")
+}
+else{
+    console.log("1 is not present")
+}
+
+for(let item of numbers1){
+    console.log(item)
+}
+
+const myArray = [1,2,3,3,4,4,,5,5,4,,,5,,5,4,3,,4,4,,4,3,3,4,,5,4,345,6,6,7,65,,7,78,,66,6,,78,7,,6,5,5,6,7,78,,,67,6,6555,,7,6,,6,]
+
+const uniqueElements = new Set(myArray); 
+console.log(uniqueElements);  
+
+let length =0; 
+for(let element of uniqueElements){
+
+    length++;
+
+}
+
+console.log(length);
 
 
 
